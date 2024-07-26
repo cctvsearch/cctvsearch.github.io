1a4cc56ed8dbcae84731c4b8d6fb1736c93e9c06
cctvsearch <cctvsearch1234@gmail.com>
2024-07-26 09:59:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -275,19 +275,16 @@ kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
     });
 });
 
-// 로드뷰 활성화 버튼 클릭 핸들러
-function enableRoadview() {
-    roadviewContainer.style.display = 'block';
-    mapContainer.style.display = 'none';
-    map.addOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);
-    map.relayout();
-}
-
-// 로드뷰 비활성화 버튼 클릭 핸들러
-function disableRoadview() {
-    roadviewContainer.style.display = 'none';
-    mapContainer.style.display = 'block';
-    map.removeOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);
+function toggleRoadview() {
+    if (roadviewContainer.style.display === 'none') {
+        roadviewContainer.style.display = 'block';
+        mapContainer.style.display = 'none';
+        map.removeOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);
+    } else {
+        roadviewContainer.style.display = 'none';
+        mapContainer.style.display = 'block';
+        map.addOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);
+    }
     map.relayout();
 }
 
