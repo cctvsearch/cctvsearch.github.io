52622dd1c7cb36a00a964ce2633cc42eae5ef26b
cctvsearch <cctvsearch1234@gmail.com>
2024-10-21 09:45:36 +0900

    Update categoryG.js

--- data/categoryG.js
+++ data/categoryG.js
@@ -30,34 +30,35 @@ var Gpositions = [
 var GInfo = [
     {
         number: "A-JA-8",
-        address: "중앙동 24-12",
+        address: "중앙동 24-12 || 향교말길 15-16",
         rotation: 1,
         fixed: 3,
-        description: "향교말길 15-16",
+        description: "",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-9",
-        address: "중앙동 25-7",
+        address: "중앙동 25-7 || 향교말길 7-4",
         rotation: 1,
         fixed: 4,
-        description: "향교말길 7-4",
+        description: "A-JA-12 (FX #2)/ A-JA-11 (FX #5)/ A-JA-21,22 (FX #6)/ A-JA-13 (FX #9)
+                      A-JA-10 (E #5)/ A-JA-6 (E #6)",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-10",
-        address: "중앙동 7-9",
+        address: "중앙동 7-9 || 향교말길 39",
         rotation: 1,
         fixed: 3,
-        description: "향교말길 39",
+        description: "A-JA-9*",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-11",
-        address: "중앙동 9-1",
+        address: "중앙동 9-1 과천외국어고등학교 앞 주택-미분류",
         rotation: 1,
         fixed: 1,
-        description: "과천외국어고등학교 앞 주택-미분류",
+        description: "A-JA-9*",
         image: "https://via.placeholder.com/150"
     },
     {
@@ -65,63 +66,64 @@ var GInfo = [
         address: "중앙동 10-8",
         rotation: 1,
         fixed: 2,
-        description: "",
+        description: "A-JA-9*",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-13",
-        address: "중앙동 11",
+        address: "중앙동 11 과천외고 앞 등산로계단 밑",
         rotation: 1,
         fixed: 0,
-        description: "과천외고 앞 등산로계단 밑",
+        description: "A-JA-9* / A-GC-85 (FX #10)",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-14",
-        address: "중앙동 33-13",
+        address: "중앙동 33-13 1단지 옆",
         rotation: 1,
         fixed: 3,
-        description: "1단지 옆",
+        description: "A-JA-17 (FX #1)/ A-JA-20 (FX #2)/ A-JA-26 (FX #5)/ A-JA-18,19 (FX #6)
+                      A-JA-15 (FX #9)/ A-JA-16 (FX #10)/ A-JA-24 (E #7)/ A-JA-23 (E #8)",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-16",
-        address: "중앙동 29",
+        address: "중앙동 29 교통경로당 놀이터",
         rotation: 1,
         fixed: 2,
-        description: "교동경로당 놀이터",
+        description: "A-JA-14*",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-17",
-        address: "중앙동 19-7",
+        address: "중앙동 19-7 자전거주차장 옆(1단지 근처)",
         rotation: 1,
         fixed: 3,
-        description: "자전거주차장 옆(1단지 근처)",
+        description: "A-JA-14*",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-18",
-        address: "중앙동 20-1",
+        address: "중앙동 20-1 과천여고 정문 앞",
         rotation: 1,
         fixed: 1,
-        description: "과천여고 정문 앞",
+        description: "A-JA-14*,19",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-20",
-        address: "중앙동 19-1",
+        address: "중앙동 19-1 || 희망4길 26",
         rotation: 1,
         fixed: 2,
-        description: "희망4길 26",
+        description: "A-JA-14*",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-21",
-        address: "중앙동 80-1",
+        address: "중앙동 80-1 관악산 등산로 입구",
         rotation: 1,
         fixed: 4,
-        description: "관악산 등산로 입구",
+        description: "A-JA-9* / A-JA-44 (FX #10)",
         image: "https://via.placeholder.com/150"
     },
     {
@@ -129,119 +131,121 @@ var GInfo = [
         address: "중앙동 14-1",
         rotation: 1,
         fixed: 3,
-        description: "",
+        description: "A-JA-14*",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-24",
-        address: "중앙동 17-4",
+        address: "중앙동 17-4 || 희망4길 57",
         rotation: 1,
         fixed: 2,
-        description: "희망4길 57",
+        description: "A-JA-14*",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-25",
-        address: "중앙동 71",
+        address: "중앙동 71 1105동~1106동 사이",
         rotation: 1,
         fixed: 1,
-        description: "1105동 ~ 1106동 사이",
+        description: "A-JA-27 (E #6)/ A-JA-25 (E #9)",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-27",
-        address: "중앙동 74번지",
+        address: "중앙동 74번지 래미안에코팰리스 1109 앞",
         rotation: 1,
         fixed: 0,
-        description: "래미안에코팰리스 1109 앞",
+        description: "A-JA-25*",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-28",
-        address: "중앙동 67",
+        address: "중앙동 67 과천초~1019 앞",
         rotation: 1,
         fixed: 3,
-        description: "과천초~1019 앞",
+        description: "광연결 안됨 (FX #1)/ P35,과천초 4R (FX #2)/ A-JA-29 (E #5)/ A-JA-30 (E #6)",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-29",
-        address: "관문동 107-2",
+        address: "관문동 107-2 과천초-건강가정지원센터 앞",
         rotation: 1,
         fixed: 3,
-        description: "과천초-건강가정지원센터 앞",
+        description: "A-JA-28*",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-30",
-        address: "중앙동 75",
+        address: "중앙동 75 과천초 후문",
         rotation: 1,
         fixed: 3,
-        description: "과천초 후문",
+        description: "A-JA-28* / A-JA-46 (FX #10)",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-31",
-        address: "중앙동 79",
+        address: "중앙동 79 스타벅스 과천성당옆",
         rotation: 1,
         fixed: 3,
-        description: "스타벅스 과천성당옆",
+        description: "",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-33",
-        address: "중앙로 129",
+        address: "중앙로 129 고려빌딩 앞",
         rotation: 1,
         fixed: 4,
-        description: "고려빌딩 앞",
+        description: "광연결 안됨 (FX #1)/ P35,과천초 4R (FX #2)/ A-JA-3,36 (E #5)
+                      A-JA-34,35 A-BY-11 (E #6)",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-35",
-        address: "중앙동 41-1",
+        address: "중앙동 41-1 KT건물 앞",
         rotation: 1,
         fixed: 3,
-        description: "KT건물 앞",
+        description: "A-JA-33*",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-36",
-        address: "중앙동 41-2",
+        address: "중앙동 41-2 119동~120동 사이 관악산 등산로길",
         rotation: 1,
         fixed: 0,
-        description: "119동~120동 사이 관악산 등산로길",
+        description: "A-JA-33*",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-38",
-        address: "관문로 136",
+        address: "관문로 136 중앙동 주민센터 앞",
         rotation: 0,
         fixed: 1,
-        description: "중앙동 주민센터 앞",
+        description: "A-GC-53 (E #5)/ A-GC-53 (E #6)",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-39",
-        address: "중앙동 67",
+        address: "중앙동 67 1007동~1002동사이",
         rotation: 1,
         fixed: 0,
-        description: "1007동~1002동사이",
+        description: "A-GC-56 (FX #1)/ A-GC-4,60 (FX #2)/ A-GC-61,62 (FX #5)/ A-GC-48 (FX #6)
+                      A-GC-59 (FX #9)/ A-GC-61,62 (FX #10)/ J60 (FX #13) / A-JA-41 (E #5)/ A=JA-40 (E #6)",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-40",
-        address: "중앙동 67",
+        address: "중앙동 67 1006동 앞 대로변",
         rotation: 1,
         fixed: 0,
-        description: "1006동 앞 대로변",
+        description: "A-JA-39*",
         image: "https://via.placeholder.com/150"
     },
     {
         number: "A-JA-41",
-        address: "중앙동 67",
+        address: "중앙동 67 1014동 옆 초소~상가사이",
         rotation: 1,
         fixed: 0,
-        description: "1014동 옆 초소~상가사이",
+        description: "A-JA-39*",
         image: "https://via.placeholder.com/150"
     }
 ];
