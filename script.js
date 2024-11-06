0c74c0e0f8479304c669d5ca01aa156c969fdf10
cctvsearch <cctvsearch1234@gmail.com>
2024-11-06 10:10:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -402,27 +402,18 @@ function toggleRoadview() {
     if (isRoadviewEnabled) {
         map.addOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);
         roadviewContainer.style.display = 'block';
-        minimapContainer.style.display = 'block';
-        
+        minimapContainer.style.display = 'block'; // 추가된 코드
         setTimeout(function() {
-            kakao.maps.event.trigger(roadview, 'resize'); // 로드뷰 강제 갱신
-            minimap.relayout();  // 미니맵 강제 리프레시
-            minimap.setCenter(map.getCenter());
+            minimap.relayout();  // minimap 강제 리프레시
+            minimap.setCenter(map.getCenter()); // minimap 중심 재설정
         }, 0);
-
-        // 카테고리 드롭다운 강제 갱신
-        document.getElementById('categoryDropdownContainer').style.display = 'block';
     } else {
         map.removeOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);
         roadviewContainer.style.display = 'none';
-        minimapContainer.style.display = 'none';
+        minimapContainer.style.display = 'none'; // 추가된 코드
         mapContainer.style.display = 'block';
-
-        // 카테고리 드롭다운 숨기기
-        document.getElementById('categoryDropdownContainer').style.display = 'none';
     }
 }
-
 
 var roadviewToggleBtn = document.getElementById('roadviewToggle');
 roadviewToggleBtn.addEventListener('click', function() {
@@ -592,6 +583,3 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
-
-
-
